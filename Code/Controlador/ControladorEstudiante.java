41aaac04afaf8bf705c17f4fad036c924287d121
MauroKuh13 <a19200637@alumnos.uady.mx>
2023-12-10 17:33:20 -0600

    Método hacer contancia

--- Code/Controlador/ControladorEstudiante.java
+++ Code/Controlador/ControladorEstudiante.java
@@ -1,221 +1,290 @@
 package Controlador;
 
-import Modelo.Consultas;
+import BD.Consultador;
 import Modelo.Estudiante;
-import Vista.NewJFrame;
+import Vista.InicioEstudiante;
+import com.itextpdf.text.BadElementException;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.ColumnText;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfPageEventHelper;
+import com.itextpdf.text.pdf.PdfWriter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import javax.swing.JTextArea;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 
-
+/**
+ *
+ * @author mauro
+ */
 public class ControladorEstudiante implements ActionListener{
-   
+    
     Estudiante estudiante;
-    NewJFrame vista;
-    Consultas consultador;
-    JTextArea textArea;
+    InicioEstudiante vista;
+    Consultador consultas;
     
-
-    //Constructor 
-    public ControladorEstudiante(NewJFrame vista, Consultas consultador, Estudiante estudiante){
-       this.estudiante=estudiante;
-       this.vista = vista;
-       this.consultador=consultador;
-       this.vista.btn_agregarIC.addActionListener(this);
-       this.vista.btn_modificar.addActionListener(this);
-       this.vista.btn_buscar.addActionListener(this);
-       this.vista.btn_limpiar.addActionListener(this);
-       this.vista.btn_mostrar.addActionListener(this);
-       
+    public ControladorEstudiante(Estudiante estudiante, InicioEstudiante vista, Consultador consultas){
+        this.estudiante = estudiante;
+        this.vista = vista;
+        this.vista.botonBuscar.addActionListener(this);
+        this.consultas=consultas;
+        this.vista.contancia_btn.addActionListener(this);
     }
-
-    //Funcion Para desplegar Menú
-    @Override
+    
+     @Override
     public void actionPerformed(ActionEvent e) {
-        
-        if (e.getSource() == vista.btn_agregarIC) {
-            AgregarEstudiante();
-        } else if (e.getSource() == vista.btn_modificar) {
-            ModificarEstudiante();
-            Mostrar();
-           limpiarCampos();
-        } else if (e.getSource() == vista.btn_buscar) {
-            BuscarEstudiante();
-        } else if (e.getSource() == vista.btn_limpiar) {
-            limpiarCampos();
-        } else if(e.getSource()==vista.btn_mostrar){
-            Mostrar();
+       
+        if (e.getSource() == vista.botonBuscar) {
+            buscar();
+        }else if(e.getSource()==vista.contancia_btn){
+            try {
+                HacerConstancia();
+            } catch (DocumentException ex) {
+                Logger.getLogger(ControladorReportesInstructor.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(ControladorReportesInstructor.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
+        
     }
-
-   public void Mostrar(){
-        consultador.MostrarAlumnos(vista.tabla2);
-    }
-
-    public void Seleccionar(JTable paramTabla, JTextField matricula, JTextField paramNombres, JTextField calif1, JTextField md2, JTextField md3, JTextField md4, JTextField cfinal){
     
-        try {
+    public void buscar(){
+        
+        llenarDesdeVista();
+   
+        if(consultas.buscar(estudiante)){
+    
+            DefaultTableModel modelo = new DefaultTableModel();
+            TableRowSorter<TableModel> OrdenarTabla = new TableRowSorter<TableModel>(modelo);
+            vista.tablaEst.setRowSorter(OrdenarTabla);
+
+            modelo.addColumn("Matricula");
+            modelo.addColumn("Nombre");
+            modelo.addColumn("Modulo 1");
+            modelo.addColumn("Modulo 2");
+            modelo.addColumn("Modulo 3");
+            modelo.addColumn("Modulo 4");
+            modelo.addColumn("Final");
+        
+            vista.tablaEst.setModel(modelo);
+        
+            String[] datos = new String[7];
+        
+            datos[0]=String.valueOf(estudiante.getMatricula());
+            datos[1]=String.valueOf(estudiante.getNombre());
+            datos[2]=String.valueOf(estudiante.getCalif1());
+            datos[3]=String.valueOf(estudiante.getCalif2());
+            datos[4]=String.valueOf(estudiante.getCalif3());
+            datos[5]=String.valueOf(estudiante.getCalif4());
+         if(estudiante.getCalif1()<=60|| estudiante.getCalif2()<=60 || estudiante.getCalif3()<=60 || estudiante.getCalif4()<=60){
+         
+              datos[6] = String.valueOf("Reprobado");
+         
+         }else{
+             datos[6] = String.valueOf("Aprobado");
+         }
+        
+                
+            modelo.addRow(datos);
+        
+            vista.tablaEst.setModel(modelo);
             
-            int fila = paramTabla.getSelectedRow();
-            
-            if(fila>=0){
-                matricula.setText((paramTabla.getValueAt(fila, 0).toString()));
-                paramNombres.setText((paramTabla.getValueAt(fila, 1).toString()));
-                calif1.setText((paramTabla.getValueAt(fila, 2).toString()));
-                cfinal.setText((paramTabla.getValueAt(fila, 6).toString()));
-                
-                
-                boolean dip=true;
-                
-                 if(Integer.parseInt(paramTabla.getValueAt(fila, 2).toString())>=70){
-                    vista.txt_estado1.setText("Aprobado");
-                 }else{
-                    vista.txt_estado1.setText("Reprobado");
-                    dip=false;
-                 }
-                 
-                 if(Integer.parseInt(paramTabla.getValueAt(fila, 3).toString())>=70){
-                    vista.txt_md2.setText("Aprobado");
-                 }else{
-                    vista.txt_md2.setText("Reprobado");
-                    dip=false;
-                 }
-                 
-                 if(Integer.parseInt(paramTabla.getValueAt(fila, 4).toString())>=70){
-                    vista.txt_md3.setText("Aprobado");
-                 }else{
-                    vista.txt_md3.setText("Reprobado");
-                    dip=false;
-                 }
-                 
-                 if(Integer.parseInt(paramTabla.getValueAt(fila, 5).toString())>=70){
-                    vista.txt_md4.setText("Aprobado");
-                 }else{
-                    vista.txt_md4.setText("Reprobado");
-                    dip=false;
-                 }
-                
-                 if(dip){
-                    vista.txt_estadoDiplomado.setText("Aprobado");
-                }else{
-                    vista.txt_estadoDiplomado.setText("Reprobado");
-                }
-                 
-            }else{
-                 JOptionPane.showMessageDialog(null, "Fila no seleccionada");
+    
+        }else{
+            JOptionPane.showMessageDialog(null, "Datos incorrectos o Usuario no existente", "Error", JOptionPane.ERROR_MESSAGE);   
+        }
+        
+
+    
+    }
+    
+    public void HacerConstancia() throws FileNotFoundException, DocumentException, BadElementException, IOException{
+ 
+        estudiante.setMatricula(Integer.parseInt(vista.tf_matricula.getText()));
+        if(consultas.buscar(estudiante)){
+        
+            if(estudiante.getCalif1()<=60|| estudiante.getCalif2()<=60 || estudiante.getCalif3()<=60 || estudiante.getCalif4()<=60){
+                JOptionPane.showMessageDialog(null, "No aprobó el diplomado", "Error", JOptionPane.ERROR_MESSAGE);   
+            return;  
             }
             
+        }else{
+          JOptionPane.showMessageDialog(null, "Matricula no encontrada", "Error", JOptionPane.ERROR_MESSAGE);   
+          return;
+        }
+        
+        String nombre = estudiante.getNombre();
+        String matricula = String.valueOf(estudiante.getMatricula());
+        String diplomado = vista.c_diplomado.getSelectedItem().toString();
+        
+        String path = "";
+        
+        JFileChooser j=new JFileChooser();
+        j.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        
+        int x= j.showSaveDialog(this.vista);
+        
+        if(x==JFileChooser.APPROVE_OPTION){
+        
+            path=j.getSelectedFile().getPath(); 
+        }
+        
+        Document doc = new Document();
+        doc.setMargins(20, 20, 20, 20); // Establecer márgenes
+        doc.addCreationDate(); // Agregar fecha de creación
+        
+        try{
+            PdfWriter writer = PdfWriter.getInstance(doc, new FileOutputStream(path+"\\constancia.pdf"));
            
-        } catch (Exception e){
-            JOptionPane.showMessageDialog(null, "Error de Selección");
-        }
-    
-    }   
-    
-    
-    
-     public void AgregarEstudiante() {
-        llenarEstudianteDesdeVista();
-        if (consultador.registrar(estudiante)) {
-            JOptionPane.showMessageDialog(null, "Estudiante agregado");
-            limpiarCampos();
-        } else {
-            JOptionPane.showMessageDialog(null, "Error al agregar");
-            limpiarCampos();
-        }
-    }
-
-    public void ModificarEstudiante() {
-        int fila =vista.tabla2.getSelectedRow();
-        estudiante.setMatricula(Integer.parseInt(vista.tabla2.getValueAt(fila, 0).toString()));
-        estudiante.setCalif1(Integer.parseInt(vista.tabla2.getValueAt(fila, 2).toString()));
-        estudiante.setCalif2(Integer.parseInt(vista.tabla2.getValueAt(fila, 3).toString()));
-        estudiante.setCalif3(Integer.parseInt(vista.tabla2.getValueAt(fila, 4).toString()));
-        estudiante.setCalif4(Integer.parseInt(vista.tabla2.getValueAt(fila, 5).toString()));
-        estudiante.setCalif(((estudiante.getCalif1()+estudiante.getCalif2()+estudiante.getCalif3()+estudiante.getCalif4())/4));
-        
-        
-        if (consultador.modificar(estudiante)) {
-            JOptionPane.showMessageDialog(null, "Estudiante modificado");
+            writer.setPageEvent(new PdfPageEventHelper() {
+            @Override
             
-            
-            if(estudiante.getCalif1()>=70){
-               vista.txt_estado1.setText("Aprobado");
-            }else{
-               vista.txt_estado1.setText("Reprobado");
+            public void onEndPage(PdfWriter writer, Document document) {
+            // Agregar el número de página y el pie de página
+            int pageNumber = writer.getPageNumber();
+            ColumnText.showTextAligned(writer.getDirectContent(),
+                    Element.ALIGN_CENTER, new Phrase("Página " + pageNumber),
+                    (document.right() + document.left()) / 2, document.bottom() - 10, 0);
             }
-          
-          
-        } else {
-            JOptionPane.showMessageDialog(null, "Error al modificar");
-            limpiarCampos();
-        }
-    }
+            });
+            
+            
+            doc.open();
+            PdfContentByte cb = writer.getDirectContent();
+            
+            // Línea azul
+            cb.setColorStroke(new BaseColor(0, 0, 255)); // Azul
+            cb.setLineWidth(2f); // Ancho de la línea
+             cb.rectangle(doc.left(), doc.bottom(), doc.right() - doc.left(), doc.top() - doc.bottom());
+             cb.stroke();
 
-    public void EliminarEstudiante() {
-        estudiante.setId(Integer.parseInt(vista.txt_id.getText()));
-        if (consultador.eliminar(estudiante)) {
-            JOptionPane.showMessageDialog(null, "Estudiante eliminado");
-            limpiarCampos();
-        } else {
-            JOptionPane.showMessageDialog(null, "Error al eliminar");
-            limpiarCampos();
+            // Línea dorada
+            cb.setColorStroke(new BaseColor(255, 215, 0)); // Dorado
+            cb.setLineWidth(2f); // Ancho de la línea
+            cb.rectangle(doc.left() + 2, doc.bottom() + 2, doc.right() - doc.left() - 4, doc.top() - doc.bottom() - 4);
+            cb.stroke();
+            
+            // Configurar el formato de fecha en español
+            SimpleDateFormat formatoFecha = new SimpleDateFormat("dd MMMM yyyy", new Locale("es", "ES"));
+
+            // Párrafo con fecha actual en español
+            Date fechaActual = new Date();
+            String fechaFormateada = formatoFecha.format(fechaActual);
+
+            Paragraph parrafoFecha = new Paragraph("Fecha: " + fechaFormateada);
+            parrafoFecha.setAlignment(Element.ALIGN_RIGHT);
+            parrafoFecha.setIndentationRight(10);
+            parrafoFecha.setFont(FontFactory.getFont("Tahoma", 12));
+            
+            //logo
+            Image logo = Image.getInstance("C:\\Users\\mauro\\Downloads\\EducacionC\\EC\\src\\main\\java\\img\\logo.jpg");
+            logo.scaleToFit(100, 100);  
+            logo.setAlignment(Chunk.ALIGN_LEFT);
+            logo.setIndentationLeft(15);
+           
+            PdfPTable table = new PdfPTable(1);
+            table.setWidthPercentage(100);
+
+            // Párrafo 1 (alineación izquierda)
+            Paragraph parrafo1 = new Paragraph();
+            parrafo1.setAlignment(Element.ALIGN_LEFT);
+            parrafo1.add("Educación Continua,");
+            parrafo1.setFont(FontFactory.getFont("Tahoma", 70, Font.NORMAL, BaseColor.BLACK));
+            parrafo1.setIndentationLeft(15);
+            parrafo1.setSpacingAfter(10);
+
+            PdfPCell cell1 = new PdfPCell(parrafo1);
+            cell1.setBorder(Rectangle.NO_BORDER);
+            cell1.setPadding(10);
+            table.addCell(cell1);
+
+            // Párrafo 2 (alineación izquierda)
+            Paragraph parrafo2 = new Paragraph();
+            parrafo2.setAlignment(Element.ALIGN_LEFT);
+            parrafo2.add("Hace constar que el(la) alumno(a) "+nombre+" con la matricula "+matricula+" ha participado satisfactoriamente en el curso/diplomado "+diplomado+" dentro del programa de Educación Continua ofrecido por nuestra institución.");
+            parrafo2.setFont(FontFactory.getFont("Tahoma", 70, Font.NORMAL, BaseColor.BLACK));
+            parrafo2.setIndentationLeft(15);
+            parrafo2.setSpacingAfter(10);
+
+            PdfPCell cell2 = new PdfPCell(parrafo2);
+            cell2.setBorder(Rectangle.NO_BORDER);
+            cell2.setPadding(10);
+            table.addCell(cell2);
+
+            // Párrafo 3 (alineación izquierda)
+            Paragraph parrafo3 = new Paragraph();
+            parrafo3.setAlignment(Element.ALIGN_LEFT);
+            parrafo3.add("Durante el desarrollo de este programa, el(la) estudiante demostró un compromiso excepcional, participación activa y un notable desempeño académico, adquiriendo los conocimientos y habilidades específicos del curso.");
+            parrafo3.setFont(FontFactory.getFont("Tahoma", 20, Font.NORMAL, BaseColor.BLACK));
+            parrafo3.setIndentationLeft(15);
+            parrafo3.setSpacingAfter(10);
+
+            PdfPCell cell3 = new PdfPCell(parrafo3);
+            cell3.setBorder(Rectangle.NO_BORDER);
+            cell3.setPadding(10);
+            table.addCell(cell3);
+
+            // Párrafo 4 (alineación centrada)
+            Paragraph parrafo4 = new Paragraph();
+            parrafo4.setAlignment(Element.ALIGN_CENTER);
+            parrafo4.add("Se extiende la presente constancia a petición del interesado, para los fines que considere pertinentes.");
+            parrafo4.setFont(FontFactory.getFont("Tahoma", 70, Font.NORMAL, BaseColor.BLACK));
+            parrafo4.setIndentationLeft(15);
+            parrafo4.setSpacingAfter(10);
+
+            PdfPCell cell4 = new PdfPCell(parrafo4);
+            cell4.setBorder(Rectangle.NO_BORDER);
+            cell4.setPadding(10);
+            table.addCell(cell4);
+            
+           Image firma = Image.getInstance("C:\\Users\\mauro\\Downloads\\EducacionC\\EC\\src\\main\\java\\img\\firma.png");
+           firma.scaleToFit(250, 250);  // Ajusta el tamaño del logo según tus necesidades
+           firma.setAlignment(Chunk.ALIGN_CENTER); 
+            
+           doc.add(new Paragraph(" ")); 
+           doc.add(parrafoFecha);
+           doc.add(logo);
+           doc.add(new Paragraph(" ")); 
+           doc.add(new Paragraph(" ")); 
+           doc.add(table);
+           doc.add(new Paragraph(" ")); 
+           doc.add(new Paragraph(" ")); 
+           doc.add(new Paragraph(" ")); 
+           doc.add(firma);
+            
+            } catch (FileNotFoundException | DocumentException ex){
+            Logger.getLogger(ControladorReportesInstructor.class.getName()).log(Level.SEVERE, null, ex);
         }
+        doc.close(); 
     }
     
-    public void BuscarEstudiante() {
-         estudiante.setMatricula(Integer.parseInt(vista.txt_matricula.getText()));
-        if (consultador.buscar(estudiante)) {
-            llenarVistaDesdeEstudiante();
-            
-            if(estudiante.getCalif1()>=70){
-               vista.txt_estado1.setText("Aprobado");
-            }else{
-               vista.txt_estado1.setText("Reprobado");
-            }
-            
-            if(estudiante.getCalif1()<70 || estudiante.getCalif1()<70 || estudiante.getCalif1()<70 || estudiante.getCalif1()<70 ){
-            
-                vista.txt_estadoDiplomado.setText("Aprobado");
-               
-            }else{
-                vista.txt_estadoDiplomado.setText("Reprobado");
-            }
-            
-            
-            int total = (estudiante.getCalif1() + estudiante.getCalif2() + estudiante.getCalif3() + estudiante.getCalif4())/4;
-            vista.txt_diplomado.setText(String.valueOf(total));
-            
-            
-        } else {
-            JOptionPane.showMessageDialog(null, "Error al buscar");
-            limpiarCampos();
-        }
+    private void llenarDesdeVista() {
+        estudiante.setMatricula(Integer.parseInt(vista.tf_matricula.getText()));
     }
     
-    
-    public void llenarEstudianteDesdeVista() {
-        
-        estudiante.setMatricula(Integer.parseInt(vista.txt_matricula.getText()));
-        estudiante.setNombre(vista.txt_nombre.getText());
-        estudiante.setCalif(Integer.parseInt(vista.txt_Calif.getText()));
-    }
-
-    public void llenarVistaDesdeEstudiante() {
-        vista.txt_matricula.setText(String.valueOf(estudiante.getMatricula()));
-        vista.txt_nombre.setText(estudiante.getNombre());
-        vista.txt_Calif.setText(String.valueOf(estudiante.getCalif()));
-        
-    }
-
-    public void limpiarCampos() {
-        vista.txt_nombre.setText("");
-        vista.txt_matricula.setText("");
-        vista.txt_Calif.setText("");
-    }
-
 }
