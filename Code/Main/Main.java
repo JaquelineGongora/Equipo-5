825a9f497700874914342218328caa2dba842af6
MauroKuh13 <a19200637@alumnos.uady.mx>
2023-12-10 17:29:10 -0600

    Correción Main

---
+++ Code/Main/Main.java
@@ -0,0 +1,26 @@
+package Main;
+import Controlador.ControladorLogin;
+import BD.Consultador;
+import Modelo.Usuario;
+import Vista.AccionesUI;
+import Vista.InicioEstudiante;
+import Vista.Login;
+/**
+ *
+ * @author margl
+ */
+public class Main {
+
+    public static void main(String[] args) {
+         
+        Login vista = new Login();
+        Usuario usuario = new Usuario();
+        AccionesUI inicio = new AccionesUI();
+        InicioEstudiante inEst= new InicioEstudiante();
+        Consultador consulta = new Consultador();
+        ControladorLogin controlador = new ControladorLogin(usuario, vista,consulta, inicio, inEst);
+        vista.setVisible(true);
+    }
+    
+}
+
