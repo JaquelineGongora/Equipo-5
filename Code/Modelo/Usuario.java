4667549fe59b08546aa37884dfbedeb6a91da355
MauroKuh13 <a19200637@alumnos.uady.mx>
2023-12-10 19:24:16 -0600

    Create Usuario.java

---
+++ Code/Modelo/Usuario.java
@@ -0,0 +1,33 @@
+package Modelo;
+
+public class Usuario {
+    
+    String nombre, contrasenia;
+    int tipo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+}
