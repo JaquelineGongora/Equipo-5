2b1b75d1d96f17b63d414f1f01a29a0fcb330902
MauroKuh13 <a19200637@alumnos.uady.mx>
2023-12-10 19:23:42 -0600

    Create Estudiante.java

---
+++ Code/Modelo/Estudiante.java
@@ -0,0 +1,54 @@
+
+package Modelo;
+
+
+public class Estudiante {
+    
+    private int id;
+    private int matricula;
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+    private String nombre;
+    private String diplomado;
+
+    public int getCalif() {
+        return calif;
+    }
+
+    public void setCalif(int calif) {
+        this.calif = calif;
+    }
+    private int calif;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDiplomado() {
+        return diplomado;
+    }
+
+    public void setDiplomado(String diplomado) {
+        this.diplomado = diplomado;
+    }
+    
+    
+}
